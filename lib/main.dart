105beeafe4522d2db64a15f17afcc8792eb15f68
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2024-05-22 17:04:17 +0200

    use ListTiles

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,20 @@ class ListViewBuilder extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: null,
+      prototypeItem: tile(0),
       itemBuilder: (context, index) {
-        return Text('Hello World $index!');
+        return tile(index);
       },
+    );
+  }
+
+  ListTile tile(int index) {
+    return ListTile(
+      title: Text('Hello World $index!'),
+      leading: const Icon(
+        Icons.circle,
+        size: 13,
+      ),
     );
   }
 }
@@ -36,7 +47,7 @@ class MainApp extends StatelessWidget {
               ),
             ),
             padding: const EdgeInsets.all(4),
-            width: 150,
+            width: 250,
             child: const ListViewBuilder(),
           ),
         ),
