d7f830dd4f046d1a7517690422250d6d05eee694
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2024-05-27 14:02:58 +0200

    update widget_test.dart to test scrolling

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,95 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_infinite_list_view/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MainApp());
+  testWidgets('ListViewBuilder smoke test', (WidgetTester tester) async {
+    // Success of the scrolling test depends on the screen size so hard-wire it
+    tester.view.physicalSize = const Size(800, 600);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('Hello World!'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(MainApp());
 
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+    var listViewBuilder = find.byType(ListViewBuilder);
+    expect(
+      listViewBuilder,
+      findsOneWidget,
+      reason: 'One ListViewBuilder should be present',
+    );
 
-    // // Verify that our counter has incremented.
-    // expect(find.text('0'), findsNothing);
-    // expect(find.text('1'), findsOneWidget);
+    var listView = find.descendant(
+      of: listViewBuilder,
+      matching: find.byType(ListView),
+    );
+    expect(
+      listView,
+      findsOneWidget,
+      reason: 'One ListView should be descendant of ListViewBuilder',
+    );
+
+    var scrollable = find.descendant(
+      of: listView,
+      matching: find.byType(Scrollable),
+    );
+    expect(
+      scrollable,
+      findsOneWidget,
+      reason: 'One Scrollable should be descendant of ListView',
+    );
+
+    const String str0 = 'Hello World 0!';
+    const String str1 = 'Hello World 100!';
+    var tile0 = find.descendant(of: scrollable, matching: find.text(str0));
+    var tile1 = find.descendant(of: scrollable, matching: find.text(str1));
+    var arrowUpward = find.byIcon(Icons.arrow_upward);
+
+    expect(
+      arrowUpward,
+      findsOneWidget,
+      reason: 'One arrow_upward icon should be found',
+    );
+
+    expect(
+      tile0,
+      findsOneWidget,
+      reason: '$str0 should be visible now',
+    );
+    expect(
+      tile1,
+      findsNothing,
+      reason: '$str1 should not be visible now',
+    );
+
+    await tester.scrollUntilVisible(
+      tile1,
+      200,
+      scrollable: scrollable,
+      duration: const Duration(milliseconds: 10),
+    );
+
+    expect(
+      tile0,
+      findsNothing,
+      reason: '$str0 should not be visible now',
+    );
+    expect(
+      tile1,
+      findsOneWidget,
+      reason: '$str1 should be visible now',
+    );
+
+    await tester.tap(arrowUpward);
+    await tester.pumpAndSettle();
+
+    expect(
+      tile0,
+      findsOneWidget,
+      reason: '$str0 should be visible now',
+    );
+    expect(
+      tile1,
+      findsNothing,
+      reason: '$str1 should not be visible now',
+    );
   });
 }
